cd0bf552039a001453b224bd563d45fe0ec1a74e
nebulalagoon <atramhenc@gmail.com>
2019-04-24 15:30:12 +0200

    Fixed some mistakes, still not sure why it doesn't work

--- queries.js
+++ queries.js
@@ -20,7 +20,7 @@ const getUsers = (request, response) => {
 const getUserById = (request, response) => {
     const id = parseInt(request.params.id);
 
-    pool.query('SELECT users WHERE id = $1', [id], (error, results) => {
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
         if (error) {
             throw error;
         };
@@ -36,7 +36,7 @@ const createUser = (request, response) => {
         if (error) {
             throw error;
         };
-        response.status(201).send(`User added with ID: ${result.insertId}`);
+        response.status(201).send(`User added with ID: ${results.insertId}`);
     })
 };
 
@@ -44,11 +44,11 @@ const updateUser = (request, response) => {
     const id = parseInt(request.params.id);
     const { name, email } = request.body;
 
-    pool.query('UPDATE users SET name=$1, email=$2 WHERE id=$3', [name,email, id], (error, results) =>{
+    pool.query('UPDATE users SET name=$1, email=$2 WHERE id=$3', [name, email, id], (error, results) =>{
         if (error) {
             throw error;
         };
-        response.status(200).send(`User modified with ID: ${id}`);
+        response.status(200).send(`User modified with ID: ${results.id}`);
     })
 };
 
@@ -60,7 +60,7 @@ const deleteUser = (request, response) => {
             throw error;
         };
 
-        response.statu(200).send(`User deleted with ID: ${id}`);
+        response.statu(200).send(`User deleted with ID: ${results.id}`);
     })
 };
 
